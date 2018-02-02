7da7deb2ecdbae08aac2a6c43af5804a45516484
RachetRalph <ralphandrade@zoho.com>
2018-02-01 20:25:21 -0700

    Setup connections to mySQL DB to process customer orders

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -4,18 +4,18 @@ var colors = require('colors');
 var prompt = require('prompt');
 
 
-var connection = mysql.createConnection({ 
-    host: "localhost", 
-    port: 3306,   
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
     // Your username   
-    user: "root",   
+    user: "root",
     // Your password   
-    password: "root",   
-    database: "BAMAZON_DB" 
-}); 
+    password: "root",
+    database: "BAMAZON_DB"
+});
 // Establishing connection with mySql server DB
-connection.connect(function(err) {  
-     if (err) throw err;   
+connection.connect(function (err) {
+    if (err) throw err;
     //  productSearch(); 
 });
 // Empty array of customers purchases 
@@ -51,14 +51,71 @@ const buy = () => {
     // The user is promted for a request 
     let products = {
         properties: {
-            ITEM_ID: { description: colors.bgGreen('Please enter the ID # of the item you wish to purchase!')},
-            STOCK_QT: { description: colors.blue('How many items would you like to purchase?')},
+            ITEM_ID: {
+                description: colors.green('Please enter the ID # of the item you wish to purchase!')
+            },
+            STOCK_QT: {
+                description: colors.blue('How many items would you like to purchase?')
+            },
         },
     };
 
     prompt.start();
 
-    
+    prompt.get(products, function (err, res) {
+
+        let purch = {
+            ITEM_ID: res.ITEM_ID,
+            STOCK_QT: res.STOCK_QT
+        };
+
+        purchases.push(purch);
+        // console.log(purch);
+        // console.log(purchases);
+
+        //connects to the mysql database and selects the item the user selected above based on the item id number entered
+        connection.query('SELECT * FROM PRODUCTS WHERE ITEM_ID=?', purchases[0].ITEM_ID, function (err, res) {
+            if (err) console.log(err, 'That item ID doesn\'t exist');
+
+            //if the stock quantity available is less than the amount that the user wanted to purchase then the user will be alerted that the product is out of stock
+            if (res[0].STOCK_QT < purchases[0].STOCK_QT) {
+                console.log('That product is out of stock!');
+                connection.end();
+
+                //otherwise if the stock amount available is more than or equal to the amount being asked for then the purchase is continued and the user is alerted of what items are being purchased, how much one item is and what the total amount is
+            } else if (res[0].STOCK_QT >= purchases[0].STOCK_QT) {
+
+                console.log('');
+
+                console.log(purchases[0].STOCK_QT + ' items purchased');
+
+                console.log(res[0].PRODUCT_NAME, + ' ' + res[0].PRICE + " / Each");
+
+                //this creates the variable SaleTotal that contains the total amount the user is paying for this total puchase
+                let saleTotal = res[0].PRICE * purchases[0].STOCK_QT;
+
+                console.log('Total: ' + saleTotal);
+
+                //this variable contains the newly updated stock quantity of the item purchased
+                let newQty = res[0].STOCK_QT - purchases[0].STOCK_QT;
+
+                // connects to the mysql database products and updates the stock quantity for the item puchased
+                connection.query("UPDATE PRODUCTS SET STOCK_QT = " + newQty + " WHERE ITEM_ID = " + purchases[0].ITEM_ID, function (err, res) {
+                    // if(err) throw err;
+                    // console.log('Problem ', err);
+                    console.log('');
+                    console.log(colors.cyan('Your order has been processed.  Thank you for shopping with us!'));
+                    console.log('');
+
+                    connection.end();
+                })
+
+            };
+
+        })
+
+    })
+
 
 
 }
